f5278fee95d83f270e9881a801682fc589d1a8d5
Senecm <58762403+Senecm@users.noreply.github.com>
2020-10-12 19:41:46 +1030

    Added

---
+++ InterestCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class InterestCalculator {
+    public static void main(String[] args) {
+        System.out.println("This calculator will calculate (who knew!) interest.");
+        Scanner Input = new Scanner(System.in);
+        System.out.print("Enter the Principle: ");
+        int P = Input.nextInt();
+        System.out.print("Enter the Rate: ");
+        int I = Input.nextInt();
+        System.out.print("Enter the Duration (in years): ");
+        int N = Input.nextInt();
+        Input.close();
+        Calculator(P, I, N);
+    }
+    public static String Calculator(int P, int I, int N) {
+        int cal = (P) * (I) * (N); 
+        String Interest = String.format("Interest = " + cal);
+        return Interest;
+    }
+}
